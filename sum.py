6e0dd6a8fc303ed2b90168407cf4e45eea96e5e0
prasannaexample <prasannaexample@gmail.com>
2014-08-01 16:50:14 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,12 @@
+# This program adds two numbers
+# Numbers are provided by the user
+
+# Store input numbers
+num1 = 15
+num2 = 10
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
